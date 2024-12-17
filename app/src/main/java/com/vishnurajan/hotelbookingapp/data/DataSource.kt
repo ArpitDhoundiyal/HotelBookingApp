012e8b202aab495ba3004d583c0ea64e4ea50bad
ArpitDhoundiyal <arpitdhoundiyal@gmail.com>
2024-12-17 10:32:14 +0530

    Initial commit

---
+++ app/src/main/java/com/vishnurajan/hotelbookingapp/data/DataSource.kt
@@ -0,0 +1,27 @@
+package com.vishnurajan.hotelbookingapp.data
+import com.vishnurajan.hotelbookingapp.R
+
+object DataSource {
+    fun loadCategories(): List<RowItem> {
+        return listOf(
+            RowItem("Taj Mahal", "", R.drawable.taj),
+            RowItem("Taj Mahal", "", R.drawable.taj),
+            RowItem("Taj Mahal", "", R.drawable.taj),
+            RowItem("Taj Mahal", "", R.drawable.taj),
+            RowItem("Taj Mahal", "", R.drawable.taj),
+            RowItem("Taj Mahal", "", R.drawable.taj),
+        )
+    }
+
+    fun loadAmenities(amenities: List<String>): List<Amenities> {
+        val fullAmenities =  listOf(
+            Amenities(R.drawable.gym, "Gym", "1"),
+            Amenities(R.drawable.parking, "Free Parking", "2"),
+            Amenities(R.drawable.restaurant, "Restaurant", "3"),
+            Amenities(R.drawable.roomservice, "Room Service", "4"),
+            Amenities(R.drawable.swimmingpool, "Swimming Pool", "5"),
+            Amenities(R.drawable.wifi, "Wifi", "6"),
+            )
+        return fullAmenities.filter { it.id in amenities }
+    }
+}
